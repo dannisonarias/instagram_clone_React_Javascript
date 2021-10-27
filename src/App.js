cc92f7d4af6503936b4b393905cf8b83d0ddd45d
Dannison Arias <dannisonarias@gmail.com>
2021-10-27 14:21:48 -0400

    add grid component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageGrid from './comps/ImageGrid';
 import Title from './comps/Title';
 import Uploadform from './comps/Uploadform';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Title/>
       <Uploadform/>
+      <ImageGrid/>
     </div>
   );
 }
